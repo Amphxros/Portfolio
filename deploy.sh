e2d9b26c609463780afcc9ffbd22059fa175304d
Amph Rubio Bellón <ampxrubio@gmail.com>
2022-11-16 13:25:45 +0100

    fixed

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,10 @@ git init
 git add -A
 git commit -m 'deploy'
 
+# if you are deploying to https://<USERNAME>.github.io
+#git push -f git@github.com:mmur07/mmur07.github.io.git main
+
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/amphxros/portfolio.git master:gh-pages
+git push -f https://github.com/mmur07/gamedev-portfolio.git master:gh-pages
 
 cd -
--- src/views/About.vue
+++ src/views/About.vue
@@ -9,7 +9,7 @@
       </div>
       <div style="margin-top: 10px;">  In my free time I like to play videogames, doing my bullet journal and watching animation series</div>
 
-      <div style="margin-top: 20px;">I've worked on  <router-link to="/game-projects">games</router-link>, on <router-link to="/other-projects"> tools</router-link>, and took part in <router-link to="/resume">a few game jams</router-link> as well.</div>
+      <div style="margin-top: 20px;">I've worked on <router-link to="/game-projects">games</router-link>, on <router-link to="/other-projects"> tools</router-link>, and took part in <router-link to="/resume">a few game jams</router-link> as well.</div>
 
       <div style="margin-top: 40px;">I'm <strong>currently looking for a job</strong> as a game programmer or tool programmer. You can reach me at <a href="mailto:ampxrubio@gmail.com">ampxrubio@gmail.com</a> or <router-link to="/contact">through here</router-link>.</div>
     </div>
